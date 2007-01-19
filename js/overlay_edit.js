7c86ab09154c8809039195180e110725cb2f51ff
bdragon <bdragon>
2007-01-19 19:45:59 +0000

    Rudimentary support for all 3 lines.

--- js/overlay_edit.js
+++ js/overlay_edit.js
@@ -9,6 +9,10 @@ Drupal.gmap.prototype.handler.overlayedit = function(elem) {
   obj.lines = [];
   obj.lines[0] = {};
   obj.lines[0].points = []; //@@@
+  obj.lines[1] = {};
+  obj.lines[1].points = []; //@@@
+  obj.lines[2] = {};
+  obj.lines[2].points = []; //@@@
 
   var binding = obj.bind('overlay_edit_mode',function() {
     // @@@
@@ -56,25 +60,18 @@ Drupal.gmap.prototype.handler.overlayedit = function(elem) {
               obj.change('point',-1);
               break;
             case 'Line1':
-              obj.lines[0].points.push(point);
-              if (obj.lines[0].overlay) {
-                obj.map.removeOverlay(obj.lines[0].overlay);
+            case 'Line2':
+            case 'Line3':
+              var l = 0;
+              if(elem.value=='Line1') l=0;
+              if(elem.value=='Line2') l=1;
+              if(elem.value=='Line3') l=2; // @@@ Obvious hack.
+              obj.lines[l].points.push(point);
+              if (obj.lines[l].overlay) {
+                obj.map.removeOverlay(obj.lines[l].overlay);
               }
-              obj.map.addOverlay(obj.lines[0].overlay = new GPolyline(obj.lines[0].points, '#000000', 5));
-
+              obj.map.addOverlay(obj.lines[l].overlay = new GPolyline(obj.lines[0].points, '#000000', 5));
               break;
-/*
-
-          map.drupal.line1points.push(point);
-          if (map.drupal.line1overlay) map.removeOverlay(map.drupal.line1overlay);
-          map.drupal.line1overlay=new GPolyline(map.drupal.line1points, map.drupal.linecolors[0], 5);
-          map.addOverlay(map.drupal.line1overlay);
-          if (map.drupal.line1string.length > 0) map.drupal.line1string += ' + ';
-          map.drupal.line1string += point.lat() + ',' + point.lng();
-          map.drupal.gmapline1 = map.drupal.line1string;
-          break;
-          */
-
           }
         }
       });
@@ -83,19 +80,22 @@ Drupal.gmap.prototype.handler.overlayedit = function(elem) {
 }
 
 Drupal.gmap.prototype.macroparts.push(function() {
-  var temp = [];
+  var temp;
   var output = '';
   if (this.vars.points) {
     if (this.vars.points.length > 0) {
       output += ' |markers=' + this.vars.points.join(' + ');
     }
   }
-  if (this.lines[0].points) {
-    if (this.lines[0].points.length > 0) {
-      for(var i=0;i<this.lines[0].points.length;i++) {
-        temp[i] = '' + this.lines[0].points[i].lat() + ',' + this.lines[0].points[i].lng();
+  for (var q=0; q<3 ; q++) {
+    temp = [];
+    if (this.lines[q].points) {
+      if (this.lines[q].points.length > 0) {
+        for(var i=0;i<this.lines[q].points.length;i++) {
+          temp[q] = '' + this.lines[q].points[i].lat() + ',' + this.lines[q].points[i].lng();
+        }
+        output += ' |line' + (q+1) + '=' + temp.join(' + ');
       }
-      output += ' |line1=' + this.lines[0].points.join(' + ');
     }
   }
   return output;
