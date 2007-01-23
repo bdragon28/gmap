065d5219c83dea814b57cabe66324ddfed9bd434
bdragon <bdragon>
2007-01-23 15:57:32 +0000

    Fix marker popups

--- js/gmap_marker.js
+++ js/gmap_marker.js
@@ -15,14 +15,11 @@ Drupal.gmap.prototype.marker.makeMarker = function(markerdef) {
   }
   var marker = new GMarker(new GLatLng(markerdef.latitude,markerdef.longitude),opts);
 
-//  if (markerdef.info) {
-
-  // @@@
-  GEvent.addListener(marker,'click',function() {
-    marker.openInfoWindowHtml('<p>FOO!!!</p>');
-  });
-    
-
+  if (markerdef.text) {
+    GEvent.addListener(marker,'click',function() {
+      marker.openInfoWindowHtml(markerdef.text);
+      });
+  }
 
   return marker;
 }
@@ -33,6 +30,9 @@ Drupal.gmap.prototype.marker.makeMarker = function(markerdef) {
 Drupal.gmap.prototype.marker.add = function(marker) {
   var obj = this;
   obj.map.addOverlay(marker);
+  if(obj.vars.behavior.autozoom) {
+    obj.bounds.extend(marker.getPoint());
+  }
 }
 
 Drupal.gmap.prototype.marker.del = function(marker) {
@@ -45,6 +45,9 @@ Drupal.gmap.prototype.handler.gmap.push(function(elem) {
 
   if(obj.vars.markers) {
     obj.bind("init",function() {
+      if (obj.vars.behavior.autozoom) {
+        obj.bounds = new GLatLngBounds(new GLatLng(obj.vars.latitude,obj.vars.longitude),new GLatLng(obj.vars.latitude,obj.vars.longitude));
+      }
 
       for (var i=0; i<obj.vars.markers.length; i++) {
         obj.marker.add.call(obj,(obj.marker.makeMarker(obj.vars.markers[i])));
@@ -56,7 +59,10 @@ Drupal.gmap.prototype.handler.gmap.push(function(elem) {
             for (var j=0; j<obj.vars.lines[i].length; j++) {
               temp.push(new GLatLng(obj.vars.lines[i][j].latitude,obj.vars.lines[i][j].longitude));
             }
-            obj.map.addOverlay(new GPolyline(temp,obj.vars.line_colors[i], 5));
+            obj.map.addOverlay(var poly = new GPolyline(temp,obj.vars.line_colors[i], 5));
+//            if (obj.vars.behavior.autozoom) {
+//              for(var k=0;k<poly.getVertexCount();k++) {
+//                obj.bounds.extend(
           }
         }
       }
