3abb42164dabe3661dd8b95230538e579c9e4e11
bdragon <bdragon>
2007-02-12 20:25:11 +0000

    Add marker manager updates and polygon handler

---
+++ js/clusterer_marker.js
@@ -0,0 +1,17 @@
+/**
+ * GMap Markers
+ * Jef Poskanzer's Clusterer.js API version
+ */
+
+Drupal.gmap.addHandler('gmap', function(elem) {
+  var obj = this;
+  obj.clusterer = new Clusterer(obj.map);
+});
+
+Drupal.gmap.marker.add = function(marker) {
+  this.clusterer.AddMarker(marker,'FIXME');
+};
+
+Drupal.gmap.marker.del = function(marker) {
+  this.clusterer.RemoveMarker(marker);
+};
---
+++ js/gmarkermanager_marker.js
@@ -0,0 +1,18 @@
+/**
+ * GMap Markers
+ * Google GMarkerManager API version
+ */
+
+Drupal.gmap.addHandler('gmap', function(elem) {
+  var obj = this;
+  obj.mm = new GMarkerManager(obj.map,{});
+});
+
+Drupal.gmap.marker.add = function(marker) {
+  
+  this.clusterer.AddMarker(marker,'FIXME');
+};
+
+Drupal.gmap.marker.del = function(marker) {
+  this.clusterer.RemoveMarker(marker);
+};
