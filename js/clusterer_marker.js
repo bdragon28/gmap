9075a8ca6461c08f80719ef0bf005013ee289559
bdragon <bdragon>
2007-03-30 01:45:34 +0000

    Some cleanup and fixup.

--- js/clusterer_marker.js
+++ js/clusterer_marker.js
@@ -4,15 +4,48 @@
  */
 /* $Id$ */
 
+/**
+ * GMap Markers
+ * GMap API version / Base case
+ */
+/* $Id$ */
+
 Drupal.gmap.addHandler('gmap', function(elem) {
   var obj = this;
-  obj.clusterer = new Clusterer(obj.map);
+
+  obj.bind('init',function() {
+    obj.clusterer = new Clusterer(obj.map);
+  });
+
+  obj.bind('addmarker',function(marker) {
+    var m = new GMarker(new GLatLng(marker.latitude,marker.longitude),marker.opts);
+    var t;
+    if (marker.title) {
+      t = marker.title;
+    }
+    marker.marker = m;
+    GEvent.addListener(m,'click',function() {
+      obj.change('clickmarker',-1,marker);
+    });
+    obj.clusterer.AddMarker(m,t);
+  });
+
+  obj.bind('delmarker',function(marker) {
+    obj.clusterer.RemoveMarker(marker.marker);
+  });
+
+  obj.bind('clearmarkers',function() {
+    // @@@ Maybe don't nuke ALL overlays?
+    obj.map.clearOverlays();
+  });
+
+  // Default marker actions.
+  obj.bind('clickmarker',function(marker) {
+    if (marker.text) {
+      marker.marker.openInfoWindowHtml(marker.text);
+    }
+    if (marker.link) {
+        open(marker.link,'_self');
+    }
+  });
 });
-
-Drupal.gmap.marker.add = function(marker) {
-  this.clusterer.AddMarker(marker,'FIXME');
-};
-
-Drupal.gmap.marker.del = function(marker) {
-  this.clusterer.RemoveMarker(marker);
-};
--- js/gmap_marker.js
+++ js/gmap_marker.js
@@ -4,60 +4,6 @@
  */
 /* $Id$ */
 
-Drupal.gmap.marker = {};
-
-Drupal.gmap.marker.init = function(map) {
-  //NA
-}
-
-Drupal.gmap.marker.makeMarker = function(markerdef,seq) {
-  var opts = {};
-  if (markerdef.tooltip) {
-    opts.title = markerdef.tooltip; // @@@
-  }
-  if (markerdef.markername) {
-    opts.icon = Drupal.gmap.getIcon(markerdef.markername,seq);
-  }
-  else {
-    opts.icon = Drupal.gmap.getIcon();
-  }
-
-  var marker = new GMarker(new GLatLng(markerdef.latitude,markerdef.longitude),opts);
-
-  if (markerdef.text) {
-    GEvent.addListener(marker,'click',function() {
-      marker.openInfoWindowHtml(markerdef.text);
-      });
-  }
-
-  // @@@ Extend this a bit.
-  if (markerdef.link) {
-    GEvent.addListener(marker,'click',function() {
-      open(markerdef.link,'_self');
-    });
-  }
-
-  return marker;
-}
-
-/**
- * Add a marker.
- */
-Drupal.gmap.marker.add = function(marker) {
-  var obj = this;
-  obj.map.addOverlay(marker);
-  if(obj.vars.behavior.autozoom) {
-    obj.bounds.extend(marker.getPoint());
-  }
-}
-
-/**
- * Remove a marker.
- */
-Drupal.gmap.marker.del = function(marker) {
-  this.map.removeOverlay(marker);
-}
-
 Drupal.gmap.addHandler('gmap', function(elem) {
   var obj = this;
 
@@ -66,6 +12,7 @@ Drupal.gmap.addHandler('gmap', function(elem) {
       obj.bounds = new GLatLngBounds(new GLatLng(obj.vars.latitude,obj.vars.longitude),new GLatLng(obj.vars.latitude,obj.vars.longitude));
     }
   });
+
   obj.bind('addmarker',function(marker) {
     var m = new GMarker(new GLatLng(marker.latitude,marker.longitude),marker.opts);
     marker.marker = m;
@@ -79,6 +26,15 @@ Drupal.gmap.addHandler('gmap', function(elem) {
     }
   });
 
+  obj.bind('delmarker',function(marker) {
+    obj.map.removeOverlay(marker.marker);
+  });
+
+  obj.bind('clearmarkers',function() {
+    // @@@ Maybe don't nuke ALL overlays?
+    obj.map.clearOverlays();
+  });
+
   // Default marker actions.
   obj.bind('clickmarker',function(marker) {
     if (marker.text) {
@@ -89,36 +45,3 @@ Drupal.gmap.addHandler('gmap', function(elem) {
     }
   });
 });
-
-/*
-// Add a gmap handler
-Drupal.gmap.addHandler('gmap', function(elem) {
-  var obj = this;
-
-  if(obj.vars.markers) {
-    obj.bind("markersready",function() {
-      if (obj.vars.behavior.autozoom) {
-        obj.bounds = new GLatLngBounds(new GLatLng(obj.vars.latitude,obj.vars.longitude),new GLatLng(obj.vars.latitude,obj.vars.longitude));
-      }
-
-      for (var i=0; i<obj.vars.markers.length; i++) {
-        Drupal.gmap.marker.add.call(obj,(Drupal.gmap.marker.makeMarker(obj.vars.markers[i],i)));
-      }
-      if (obj.vars.lines && obj.vars.lines.length>0) {
-        for (var i=0; i<obj.vars.lines.length; i++) {
-          var temp = [];
-          if (obj.vars.lines[i] && obj.vars.lines[i].length>1) {
-            for (var j=0; j<obj.vars.lines[i].length; j++) {
-              temp.push(new GLatLng(obj.vars.lines[i][j].latitude,obj.vars.lines[i][j].longitude));
-            }
-            obj.map.addOverlay(new GPolyline(temp,obj.vars.line_colors[i], 5));
-//            if (obj.vars.behavior.autozoom) {
-//              for(var k=0;k<poly.getVertexCount();k++) {
-//                obj.bounds.extend(
-          }
-        }
-      }
-    });
-  }
-});  
-*/
